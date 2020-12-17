1b4ea41b37a73b14577ecd29e46c9381836676f8
Spotky1004 <spotky1004@gmail.com>
2020-12-17 11:09:51 +0900

    add 5 new upgrade

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function screenUpdate() {
   for (var i = 0; i < quarks.length; i++) {
     if (
       (quarks[i].driction && (Math.abs(quarks[i].position[0]) > maxCanvasPos[0] || Math.abs(quarks[i].position[1]) > maxCanvasPos[1])) ||
-      (!quarks[i].driction && Math.abs(quarks[i].position[0]) < 0.005 && Math.abs(quarks[i].position[1]) < 0.005)
+      (!quarks[i].driction && Math.abs(quarks[i].position[0]) < 0.02 && Math.abs(quarks[i].position[1]) < 0.02)
     ) {
       if (quarks[i].driction) {
         quarkBump(quarks[i].mass);
@@ -51,25 +51,29 @@ function screenUpdate() {
       i--;
       continue;
     }
-    c.beginPath();
-    c.fillStyle = quarks[i].color;
-    c.strokeStyle = quarks[i].color;
     quarks[i].update();
-    c.arc(
-      canvasSize*(0.5+quarks[i].position[0]/2)+Math.max(0, (canvas.width-canvasSize)/2),
-      canvasSize*(0.5+quarks[i].position[1]/2)+Math.max(0, (canvas.height-canvasSize)/2),
-      ((Math.log(quarks[i].mass, 10)+1)**2)*canvasSize/2000,
-      0, Math.PI*2
-    );
-    c.fill();
-    c.stroke();
+    if (i < 3000) {
+      c.beginPath();
+      c.fillStyle = quarks[i].color;
+      c.strokeStyle = quarks[i].color;
+      c.arc(
+        canvasSize*(0.5+quarks[i].position[0]/2)+Math.max(0, (canvas.width-canvasSize)/2),
+        canvasSize*(0.5+quarks[i].position[1]/2)+Math.max(0, (canvas.height-canvasSize)/2),
+        ((Math.log(quarks[i].mass, 10)+1)**2)*canvasSize/2000,
+        0, Math.PI*2
+      );
+      c.fill();
+      c.stroke();
+    }
   }
 }
 
 // saveload
 var tempSaveData = {
   "mass": 1e15,
+  "totalMass": 0,
   "quark": 0,
+  "totalQuark": 0,
   "bestRecord": 0,
   "tickSpent": 0,
   "energy": 0,
@@ -97,9 +101,12 @@ function gameReset() {
   save();
 }
 
-// calculate
+// etc
 function signRand() {
   return Math.sign(Math.random()*2-1);
+}
+function notation(num) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 // quark
@@ -113,7 +120,7 @@ class Quark {
   }
 
   update() {
-    this.speed = (this.speed/100+0.003)*(1.5);
+    this.speed = (this.speed/100+0.003)*(1.5)/Math.log(this.mass+1, 3)**2*getUpgradeEffect(4);
     var deg = (Math.atan2(this.position[1]-0, this.position[0]-0)+Math.PI*(3/2+this.driction))%(Math.PI*2);
     this.position[0] += Math.sin(deg)*this.speed;
     this.position[1] -= Math.cos(deg)*this.speed;
@@ -121,36 +128,182 @@ class Quark {
 }
 
 // game
+var upgradeCut = [10, 100, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9];
+var upgradeName = [
+  "More Quarks", "Heavier Quark", "Fast Clicler", "More Heavier Quarks", "Fasrer Quarks",
+  "Critical Chance", "Compress Quarks"
+];
+var upgradeDesc = [
+  "Spawn more Quarks per clcik", "Spawn heavier Quarks", "Autoclick Faster", "Spawn more heavier Quarks", "Faster move speed of Quarks",
+  "Increase mass! Increase speed!", "Compress some Quarks, but gain mass"
+];
 function mainDomUpdate() {
-  document.getElementById("quarkCount").innerHTML = game.quark;
+  document.getElementById("quarkCount").innerHTML = notation(game.quark);
   document.getElementById("quarkCount").style.transform = `scale(1, ${Math.max(1, Number(document.getElementById("quarkCount").style.transform.replace(/[scale\(\)]|(1, )/g, ''))*0.99)})`;
+}
+function upgradeSpawn() {
+  var unlocked = upgradeCut.length;
+  for (var i = 0, l = upgradeCut.length; i < l; i++) {
+    if (upgradeCut[i] > game.totalQuark) {
+      unlocked = i;
+      break;
+    }
+  }
+  while (document.querySelectorAll("#upgradeArea > .upgrade").length < unlocked) {
+    var parentNode = document.getElementById("upgradeArea");
+    var childNode = document.createElement("div");
+    childNode.classList.add("upgrade");
+    parentNode.append(childNode);
+
+    var upgIdx = document.querySelectorAll("#upgradeArea > .upgrade").length-1;
+
+    childNode.onclick = new Function(`buyUpgrade(${upgIdx})`);
+
+    var parentNode = document.querySelector(`#upgradeArea > .upgrade:nth-child(${upgIdx+1})`);
+    var childNode = document.createElement("div");
+    childNode.innerHTML = upgradeName[upgIdx];
+    childNode.classList.add("upgradeName");
+    parentNode.append(childNode);
+
+    var parentNode = document.querySelector(`#upgradeArea > .upgrade:nth-child(${upgIdx+1})`);
+    var childNode = document.createElement("div");
+    childNode.classList.add("upgradeEffect");
+    parentNode.append(childNode);
+
+    var parentNode = document.querySelector(`#upgradeArea > .upgrade:nth-child(${upgIdx+1}) > .upgradeEffect`);
+    var childNode = document.createElement("span");
+    childNode.classList.add("upgradeEffectNow");
+    parentNode.append(childNode);
+
+    var parentNode = document.querySelector(`#upgradeArea > .upgrade:nth-child(${upgIdx+1}) > .upgradeEffect`);
+    var childNode = document.createElement("span");
+    childNode.classList.add("upgradeEffectNext");
+    parentNode.append(childNode);
+
+    var parentNode = document.querySelector(`#upgradeArea > .upgrade:nth-child(${upgIdx+1})`);
+    var childNode = document.createElement("div");
+    childNode.innerHTML = upgradeDesc[upgIdx];
+    childNode.classList.add("upgradeDesc");
+    parentNode.append(childNode);
+
+    var parentNode = document.querySelector(`#upgradeArea > .upgrade:nth-child(${upgIdx+1})`);
+    var childNode = document.createElement("div");
+    childNode.classList.add("upgradeCost");
+    parentNode.append(childNode);
+  }
+}
+function displayUpgrade() {
+  upgradeSpawn();
+  for (var i = 0, l = document.querySelectorAll("#upgradeArea > .upgrade").length; i < l; i++) {
+    document.querySelector(`#upgradeArea > .upgrade:nth-child(${i+1}) > .upgradeCost`).innerHTML = `${notation(getUpgradeCost(i))} Quarks`;
+    if (game.quark >= getUpgradeCost(i)) {
+      document.querySelector(`#upgradeArea > .upgrade:nth-child(${i+1}) > .upgradeCost`).style.filter = `brightness(1.7)`;
+    } else {
+      document.querySelector(`#upgradeArea > .upgrade:nth-child(${i+1}) > .upgradeCost`).style.filter = `brightness(1)`;
+    }
+    document.querySelector(`#upgradeArea > .upgrade:nth-child(${i+1}) > .upgradeEffect > .upgradeEffectNow`).innerHTML = getUpgradeEffectString(i);
+    document.querySelector(`#upgradeArea > .upgrade:nth-child(${i+1}) > .upgradeEffect > .upgradeEffectNext`).innerHTML = getUpgradeEffectString(i, game.quarkUpgrade[i]+1);
+  }
 }
 function quarkBump(count) {
   game.quark += count;
+  game.totalQuark += count;
   document.getElementById("quarkCount").style.transform = `scale(1, 1.5)`;
 }
 function blackholeBump(count) {
   game.mass -= count;
+  game.totalMass += count;
 }
 function spawnQuark(count=1) {
   for (var i = 0; i < count; i++) {
     var mass = getQuarkMass();
-    var p = [(maxCanvasPos[0]*Math.random()*0.5)*signRand(), (maxCanvasPos[1]*Math.random()*0.5)*signRand()];
+    var dist = Math.random();
+    var deg = Math.PI*2*Math.random()
+    var p = [
+      Math.sin(deg)*dist,
+      -Math.cos(deg)*dist
+    ];
     quarks.push(new Quark({position: [p[0], p[1]], mass: mass, color: '#e1f0d8'}));
     quarks.push(new Quark({position: [p[0], p[1]], mass: mass, driction: 1, color: '#e6aae5'}));
+  }
+}
+function buyUpgrade(idx) {
+  if (game.quark >= getUpgradeCost(idx)) {
+    game.quark -= getUpgradeCost(idx);
+    game.quarkUpgrade[idx]++;
+    displayUpgrade();
   }
 }
 
 // get
 function getQuarkMassRange() {
-  var range = [1, 1];
+  var range = [1, (1+getUpgradeEffect(1))*getUpgradeEffect(3)];
   return range;
 }
 function getQuarkMass() {
   return Math.floor(Math.random()*(getQuarkMassRange()[1]-getQuarkMassRange()[0]+1))+getQuarkMassRange()[0];
 }
 function getClickMult() {
-  return 1;
+  return 1+getUpgradeEffect(0);
+}
+function getUpgradeCost(idx, lv=game.quarkUpgrade[idx]) {
+  var lvp = lv+1;
+  switch (idx) {
+    case 0:
+    return Math.floor(10*(lvp**lvp)*lvp);
+      break;
+    case 1:
+    return Math.floor(100*(2+lv/11)**(lv/1.6));
+      break;
+    case 2:
+    return Math.floor(1e4*(2+lv/8)**(lv*0.9));
+      break;
+    case 3:
+    return Math.floor(1e5*(1.7+lv/10)**(lv/1.2));
+      break;
+    case 4:
+    return Math.floor(1e6*(1+lv/16)**(lv/2));
+      break;
+    default:
+      return 9.99e99;
+  }
+}
+function getUpgradeEffect(idx, lv=game.quarkUpgrade[idx]) {
+  switch (idx) {
+    case 0:
+    return lv;
+      break;
+    case 1:
+    return lv**2;
+      break;
+    case 2:
+    return 1+lv**1.5/10+lv/2;
+      break;
+    case 3:
+    return 1+lv*(1+lv/10);
+      break;
+    case 4:
+    return 1+lv/(10*0.996**lv);
+      break;
+    default:
+      return 0;
+  }
+}
+function getUpgradeEffectString(idx, lv=game.quarkUpgrade[idx]) {
+  var s = getUpgradeEffect(idx, lv).toString();
+  switch (idx) {
+    case 0: case 1:
+    return `+${notation(s)}`;
+      break;
+    case 2:
+    return `${Number(s).toFixed(2)} / sec`;
+      break;
+    case 3: case 4:
+    return `x${Number(s).toFixed(2)}`;
+      break;
+    default:
+    return 'Error!';
+  }
 }
 
 // event
@@ -167,16 +320,20 @@ function getMousePos(event) {
 canvas.onclick = new Function("spawnQuark(getClickMult())");
 
 // loop
-var tickSpeed = 15;
+var tickSpeed = 20;
+var autoClickCharge = 0;
 setInterval( function () {
   screenUpdate();
+  displayUpgrade();
   sessionTickSpent++;
   game.tickSpent++;
   if (sessionTickSpent%100 === 0) {
     save();
   }
-  if (sessionTickSpent%Math.floor(1000/tickSpeed) == 0) {
-    spawnQuark(1);
+  autoClickCharge += getUpgradeEffect(2)*tickSpeed/1000;
+  if (autoClickCharge > 1) {
+    spawnQuark(getClickMult()*Math.floor(autoClickCharge));
+    autoClickCharge -= Math.floor(autoClickCharge);
   }
   mainDomUpdate();
 }, tickSpeed);
