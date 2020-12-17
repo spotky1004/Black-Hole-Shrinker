34da970e58b5f2dc0d65fa4d0bc383d2a37fe722
Spotky1004 <spotky1004@gmail.com>
2020-12-17 11:32:24 +0900

    fix upgrade 5

--- index.js
+++ index.js
@@ -262,7 +262,7 @@ function getUpgradeCost(idx, lv=game.quarkUpgrade[idx]) {
     return Math.floor(1e5*(1.7+lv/10)**(lv/1.2));
       break;
     case 4:
-    return Math.floor(4e5*(1+lv/16)**(lv/2));
+    return Math.floor(4e5*(1+lv/13)**(lv/1.6));
       break;
     default:
       return 9.99e99;
@@ -283,7 +283,7 @@ function getUpgradeEffect(idx, lv=game.quarkUpgrade[idx]) {
     return 1+lv*(1+lv/10);
       break;
     case 4:
-    return 1+lv/(10*0.996**lv);
+    return 1+lv/(10*0.9**lv);
       break;
     default:
       return 0;
